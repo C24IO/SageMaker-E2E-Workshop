59a36d27d32edaa1f18edf9217f5e3908adf1656
Chaitanya Hazarey <c@24.io>
2018-07-01 05:16:01 +0000

    Update conda and update readme with a pin

--- configs/Lifecycle_configurations.start.sh
+++ configs/Lifecycle_configurations.start.sh
@@ -8,8 +8,9 @@ echo "Creating fast.ai conda enviornment"
 cat > /home/ec2-user/fastai-setup.sh << EOF
 #!/bin/bash
 cd /home/ec2-user/SageMaker/fastai
+conda update -y -n base conda
 conda env update
-conda update -n base conda
+conda install boto3
 source activate fastai
 echo "Finished creating fast.ai conda environment"
 EOF
